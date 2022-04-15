990a3ac485af18754896637aa8218597f111401b
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-04-15 16:30:08 -0300

    feat: (collision): creating collision function

--- js/script.js
+++ js/script.js
@@ -112,7 +112,24 @@ function moveAlien(alien) {
     }, 30);
 }
 
-
+//função para  colisão
+function checkLaserCollision(laser, alien) {
+    let laserTop = parseInt(laser.style.top);
+    let laserLeft = parseInt(laser.style.left);
+    let laserBottom = laserTop - 20;
+    let alienTop = parseInt(alien.style.top);
+    let alienLeft = parseInt(alien.style.left);
+    let alienBottom = alienTop - 30;
+    if(laserLeft != 340 && laserLeft + 40 >= alienLeft) {
+        if(laserTop <= alienTop && laserTop >= alienBottom) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
 
 
 
