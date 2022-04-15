ed13a1324be03734afe3d1546ec691a59cdaaae9
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-04-15 16:17:56 -0300

    feat:(moveAliens): creating the function.

--- js/script.js
+++ js/script.js
@@ -96,4 +96,20 @@ function createAliens() {
     moveAlien(newAlien);
 }
 
+//função para movimentar os Aliens
+function moveAlien(alien) {
+    let moveAlienInterval = setInterval(() => {
+        let xPosition = parseInt(window.getComputedStyle(alien).getPropertyValue('left'));
+        if(xPosition <= 50) {
+            if(Array.from(alien.classList).includes('dead-alien')) {
+                alien.remove();
+            } else {
+                gameOver();
+            }
+        } else {
+            alien.style.left = `${xPosition - 4}px`;
+        }
+    }, 30);
+}
+
 window.addEventListener('keydown')
