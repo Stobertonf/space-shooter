91588045167a9b13027fecafc49831c5d3cd988b
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-04-15 14:31:35 -0300

    Starting the course

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const yourShip = document.querySelector('.placeholder-shoter');
+const playArea = document.querySelector('#main-play-area');
+
+
+//movimento e tiro da nave
+function flyAhip(event){
+    if(event.key == 'ArrowUp'){
+        event.preventDefault();
+        moveUp();
+    } else if(event.key == 'ArrowDown'){
+        event.preventDefault();
+        moveDown();
+    } else if(event.key == " "){
+        event.preventDefault();
+        fireLaser();
+    }
+
+//função de subir
+function moveUp(){
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top'); //devolve o código css
+    if(topPosition === "0px"){
+        return
+    } else{
+        let position = parseInt(topPosition);
+        position -= 50;
+        yourShip.style.top = '${position}px'
+    }
+ }
+
+ //função de descer
+
+function moveDown(){
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top'); //devolve o código css
+}
+   
+}
